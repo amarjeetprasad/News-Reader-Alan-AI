14896e19c55c03fa1b33f29593040e863f82c38d
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2020-08-25 03:16:11 +0530

    Updates

---
+++ precache-manifest.3cdbd3dba52277308f0be4446f6bb049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c83f4e2d5eba7b9e43afea04943cc8",
+    "url": "/News-Reader-Alan-AI/index.html"
+  },
+  {
+    "revision": "372569fd6f84884a0759",
+    "url": "/News-Reader-Alan-AI/static/css/main.2c95ec22.chunk.css"
+  },
+  {
+    "revision": "88e8f5b6e52e119a1d08",
+    "url": "/News-Reader-Alan-AI/static/js/2.b3365120.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/News-Reader-Alan-AI/static/js/2.b3365120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372569fd6f84884a0759",
+    "url": "/News-Reader-Alan-AI/static/js/main.c338a071.chunk.js"
+  },
+  {
+    "revision": "e5375d293528d5805ac5",
+    "url": "/News-Reader-Alan-AI/static/js/runtime-main.9a314bd1.js"
+  }
+]);
